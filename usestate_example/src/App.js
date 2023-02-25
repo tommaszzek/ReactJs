1cbd61b2b3d11f22557570a829ae8bab4c0df091
Tomek <tomasz.m@elektryk.edu.pl>
2023-02-25 17:34:00 +0100

    licznik o jeden

--- usestate_example/src/App.js
+++ usestate_example/src/App.js
@@ -1,9 +1,26 @@
+import {useState} from 'react';
 import './App.css';
 
+
 function App() {
+ const [count, setCount] = useState(0);
+ const handleAdd=()=>{
+  setCount(count+1);
+ }
+ const handleSub=()=>{
+  setCount(count-1);
+ }
+ const handleReset=()=>{
+  setCount(0);
+ }
   return (
     <div className="App">
-     <h1>WITAJ</h1>
+     <h1>WITAJ    </h1>
+     <h2>{count}</h2>
+     <button onClick={handleAdd} className='add'>Ddodaj</button>
+     <button onClick={handleSub} className='sub'>Odejmuj</button>
+     <button onClick={handleReset} className='reset'>Reset</button>
+   
     </div>
   );
 }
