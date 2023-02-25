9b24cc7e3464805bb41163d996909738d6c29a36
Tomek <tomasz.m@elektryk.edu.pl>
2023-02-25 21:30:02 +0100

    dodanie i usuwanie elemento z to do

--- usestate_example/src/App.js
+++ usestate_example/src/App.js
@@ -3,26 +3,65 @@ import './App.css';
 
 
 function App() {
+  let index=0;
+
+
  const [zadania, setZadanie] = useState([
-  {id:1,nazwa:'Wyjść z psem ' , status:false},
-  {id:2,nazwa:'Nauczyć się na biologię ' , status:false},
-  {id:3,nazwa:'Napisać projekt z react ' , status:true},
-  {id:4,nazwa:'Klasówka z chemi ' , status:false}
+  {id:index++,nazwa:'Wyjść z psem ' , status:false},
+  {id:index++,nazwa:'Nauczyć się na biologię ' , status:false},
+  {id:index++,nazwa:'Napisać projekt z react ' , status:true},
+  {id:index++,nazwa:'Klasówka z chemi ' , status:false}
 
  ]);
+ const [name, setName] = useState('');
+ const [status, setStatus] = useState(true);
+
+
 const handleDelete=(id)=>{
   setZadanie(zadania.filter(zad=>zad.id !==id));
 }
  
+ const handleAdd=(id)=>{
+
+  zadania.map((zad)=>{if(zad.id===id) zad.status=true});
+  
+  setZadanie(zadania);
+  }
   return (
     <div className="App">
      <h1>WITAJ    </h1>
+     <hr/>
+     <h2> Dodaj zaanie :</h2>
+     <input
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <button 
+      onClick={() => {
+        setName('');
+        zadania.push({
+          id:index++,
+          nazwa: name,
+          status:false
+        })
+      
+      }
+      
+    }
+      className='add'
+      >Add</button>
+      <hr/>
      <h2>Oto twoje zadania :</h2>
+     <button onClick={()=>setStatus(!status)} className='sub'>{status ? `Ukryj` :`Pokaz`} </button>
+     
     <ul>
-      {zadania.map((zad)=>(
-        <li key={zad.id}>
+      {status && zadania.map((zad)=>(
+        <li key={zad.id} className={zad.status ? `complete` :`incomplete`}>
           {zad.id} - treść : {zad.nazwa}
           <button onClick={()=>handleDelete(zad.id)} className='sub'>Usuń</button>
+          <button 
+          onClick={()=>handleAdd(zad.id)} 
+          className='add'>Zrobione</button>
         </li>
       ))}
 
