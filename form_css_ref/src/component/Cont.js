64c82476242279c643ff0f090eeaa1a1fee55fa6
Tomek <tomasz.m@elektryk.edu.pl>
2023-02-28 13:53:07 +0100

    Koniec użycia useRefa

--- form_css_ref/src/component/Cont.js
+++ form_css_ref/src/component/Cont.js
@@ -1,24 +1,27 @@
 import { useState } from 'react';
+import { useRef } from 'react';
 import './Cont.css';
 
 export const Cont = () => {
   const [input1, setinput1] = useState();
   const [input2, setinput2] = useState();
-
+ const inp1 = useRef();
+ const inp2 = useRef();
 
   const handelText=(e)=>{
     if(e.target.id==='data_1'){
-      console.log(e.target.value);
-      setinput1(e.target.value);
+      console.log(inp1.current.value);
+      // setinput1(e.target.value);
     }
     if(e.target.id==='data_2'){
-      console.log(e.target.value);
-      setinput2(e.target.value);
+      console.log(inp2.current.value);
+      // setinput2(e.target.value);
     }
   }
   const hendelSubmit=(e)=>{
     e.preventDefault();
-
+    setinput1(inp1.current.value);
+    setinput2(inp2.current.value);
   }
   return (
     <>
@@ -29,12 +32,14 @@ export const Cont = () => {
       name='dane1'
       id='data_1'
       onChange={handelText}
+      ref={inp1}
       />
       <input 
       type='text'
       name='dane2'
       id='data_2'
       onChange={handelText}
+      ref={inp2}
       />
       <button type='submit'>
         zatwierdz
