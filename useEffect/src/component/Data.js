8bcb48443015dc3bc6edca318a7772a55e75e8bd
Tomek <tomasz.m@elektryk.edu.pl>
2023-02-28 14:03:48 +0100

    start Use Effect_use Collbal wlasny hook

---
+++ useEffect/src/component/Data.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+export const data = () => {
+    // const [user_s, setuser_s] = useState(initialState)    
+
+    // console.log(user_s)
+    // const users='https://jsonplaceholder.typicode.com/users';
+    // fetch(users).then(response=>response.json()).then(data=>setuser_s(data));
+ 
+
+  return (
+
+    <div>data</div>
+   
+
+  )
+}
