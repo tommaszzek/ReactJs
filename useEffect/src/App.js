eb73a442621f268a1ccd8aeb58ea958bcaba0274
Tomek <tomasz.m@elektryk.edu.pl>
2023-02-28 14:16:37 +0100

    MOntowanie demontowanie komponenetu

--- useEffect/src/App.js
+++ useEffect/src/App.js
@@ -1,9 +1,11 @@
+import {Data} from './component/Data';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <h1>WITAJ</h1>
+    <Data/>
     </div>
   );
 }
--- useEffect/src/component/Data.js
+++ useEffect/src/component/Data.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import {useEffect} from 'react';
 
 
-export const data = () => {
+export const Data = () => {
     // const [user_s, setuser_s] = useState(initialState)    
 
     // console.log(user_s)
     // const users='https://jsonplaceholder.typicode.com/users';
     // fetch(users).then(response=>response.json()).then(data=>setuser_s(data));
+    useEffect(() => {
+        console.log('Montowanie')
+        return console.log('Demontowanie Montowanie');
+    }, []);   
  
 
   return (
