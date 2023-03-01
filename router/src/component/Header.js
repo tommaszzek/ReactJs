40b9d577783c4b9231eeb6f52c1130b188134395
Tomek <tomasz.m@elektryk.edu.pl>
2023-03-01 18:03:36 +0100

    Proste linki

--- router/src/component/Header.js
+++ router/src/component/Header.js
@@ -1,7 +1,18 @@
+import {Link,NavLink} from 'react-router-dom';
 import React from 'react'
+import '../css/heaer.css';
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <header>
+    <Link to='/'>Logo i slogan reklamowy strony</Link>
+    </header>
+    <nav className='navigation'>
+        <NavLink to='/' className="link" end>Start</NavLink>
+        <NavLink to='/product' className="link" end>Produkty</NavLink>
+        <NavLink to='/contact' className="link" end>Kontakt</NavLink>
+    </nav>
+    </>
   )
 }
